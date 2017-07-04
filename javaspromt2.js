6dfdad76a76a73158abb5bb105a7d9c1b8935969
kkhalifah <karen.khalifah@gmail.com>
2017-07-04 19:01:26 -0400

    basic changes

--- javaspromt2.js
+++ javaspromt2.js
@@ -1,7 +1,7 @@
 function myFunction() {
-    var person = prompt("Please enter your name", "John Smith");    
+    var person = prompt("Please enter your name", "John Smith");
     if (person != null) {
         document.getElementById("person").innerHTML =
         "Hi there " + person + "! Hope you're having a great day!";
-    }
+    } 
 }
